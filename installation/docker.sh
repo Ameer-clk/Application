0974659df3d0f1c86328d5001817d845b12780e0
Ameer ALi <114929844+Ameer-clk@users.noreply.github.com>
2023-07-02 08:21:07 +0000

    docker installation file updated

--- installation/docker.sh
+++ installation/docker.sh
@@ -1,28 +1,62 @@
 #!/bin/bash
 
-# Update the package index
-sudo apt update
+# Check the Linux distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+else
+    echo "Unsupported Linux distribution."
+    exit 1
+fi
 
-# Install necessary dependencies
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+# Install Docker on Ubuntu
+install_docker_ubuntu() {
+    # Update the apt package index
+    sudo apt update
 
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    # Install necessary packages to allow apt to use a repository over HTTPS
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Add Docker repository
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # Add the Docker GPG key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Update the package index again
-sudo apt update
+    # Add the Docker repository
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install Docker
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+    # Update the apt package index (again)
+    sudo apt update
 
-# Add the current user to the "docker" group
-sudo usermod -aG docker $USER
+    # Install Docker
+    sudo apt install -y docker.io
+}
 
-# Enable Docker service to start on boot
-sudo systemctl enable docker
+# Install Docker on Red Hat-based systems
+install_docker_rhel() {
+    # Install necessary packages
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
-# Display Docker version
+    # Set up the Docker repository
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+    # Install Docker
+    sudo yum install -y docker
+
+    # Start and enable Docker service
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    
+}
+
+# Install Docker based on the Linux distribution
+if [ "$OS" == "ubuntu" ]; then
+    install_docker_ubuntu
+elif [ "$OS" == "rhel" ]; then
+    install_docker_rhel
+else
+    echo "Unsupported Linux distribution."
+    exit 1
+fi
+
+# Verify Docker installation
 docker version
+
